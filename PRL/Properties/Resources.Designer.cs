17b158140228b38ec81c7be183ff63d1d76061f9
Phuc-Grut <ngotronghl52@gmail.com>
2024-07-19 17:36:10 +0700

    loginupdate

--- PRL/Properties/Resources.Designer.cs
+++ PRL/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace PRL.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap apple {
+            get {
+                object obj = ResourceManager.GetObject("apple", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap apple1 {
             get {
                 object obj = ResourceManager.GetObject("apple1", resourceCulture);
