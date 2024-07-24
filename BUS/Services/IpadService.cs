e680ae8de0a804025c5b3158b5014c2f6e7f2086
HUNTERDEAD2004 <149474145+HUNTERDEAD2004@users.noreply.github.com>
2024-07-24 19:52:34 +0700

    service for 7 entity

---
+++ BUS/Services/IpadService.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Models;
+using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.Services
+{
+    public class IpadService
+    {
+        private readonly AppDbContext _context;
+        IpadRepository _res;
+
+        public IpadService(AppDbContext context)
+        {
+            _context = context;
+            _res = new IpadRepository(context);
+        }
+
+        public async Task AddIpad(Ipad ipad)
+        {
+            await _res.AddIpad(ipad);
+        }
+
+        public async Task<IEnumerable<Ipad>> GetAllIpad()
+        {
+            return await _res.GetAllIpad();
+        }
+
+        public async Task<Ipad> GetByIdIpad(Guid id)
+        {
+            return await _res.GetByIdIpad(id);
+        }
+
+        public async Task RemoveIpad(Ipad ipad)
+        {
+            await _res.RemoveIpad(ipad);
+        }
+
+        public async Task UpdateIpad(Ipad ipad)
+        {
+            await _res.UpdateIpad(ipad);
+        }
+    }
+}
