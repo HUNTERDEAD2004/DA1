86589f0deddc85c41dd10dbbe21847159d5bb289
Phuc-Grut <ngotronghl52@gmail.com>
2024-07-24 20:56:58 +0700

    MacbookService,OderService

---
+++ BUS/Services/MacBookService.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.Services
+{
+    public class MacBookService
+    {
+        private readonly AppDbContext _context;
+        MacBookRepository _res;
+
+        public MacBookService(AppDbContext context)
+        {
+            _context = context;
+            _res = new MacBookRepository(context);
+        }
+
+        public async Task<MacBook> GetByIdAccessory(Guid id)
+        {
+            return await _res.GetByIdMacBook(id);
+        }
+
+        //Getall
+        public async Task<IEnumerable<MacBook>> GetAllMacBook()
+        {
+            return await _res.GetAllMacBook();
+        }
+
+        //add
+        public async Task AddMacBook(MacBook macBook)
+        {
+            await _res.AddMacBook(macBook);
+        }
+
+        //update
+        public async Task UpdateMacBook(MacBook macBook)
+        {
+            await _res.AddMacBook(macBook);
+        }
+
+        //Remove
+
+        public async Task RemoveMacBook(MacBook macBook)
+        {
+            await _res.AddMacBook(macBook);
+        }
+
+        //sử dụng await với các phương thức bất đồng bộ 
+    }
+}
---
+++ BUS/Services/OderService.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.Services
+{
+    public class OderService
+    {
+        private readonly AppDbContext _context;
+        OderRepository _res;
+
+        public OderService(AppDbContext context)
+        {
+            _context = context;
+            _res = new OderRepository(context);
+        }
+
+        public async Task<Oder> GetByIdOder(Guid id)
+        {
+            return await _res.GetByIdOder(id);
+        }
+
+        //Getall
+        public async Task<IEnumerable<Oder>> GetAllOder()
+        {
+            return await _res.GetAllOder();
+        }
+
+        //add
+        public async Task AddOder(Oder oder)
+        {
+            await _res.AddOder(oder);
+        }
+
+        //update
+        public async Task UpdateOder(Oder oder)
+        {
+            await _res.UpdateOder(oder);
+        }
+
+        //Remove
+
+        public async Task RemoveOder(Oder oder)
+        {
+            await _res.RemoveOder(oder);
+        }
+
+        //sử dụng await với các phương thức bất đồng bộ 
+    }
+}
